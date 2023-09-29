e611095903e85e4a50481560da42713f0d2528d8
Smith Sopp <smithsopp@gmail.com>
2023-09-28 19:28:47 -0600

    Rename CardManager to DeckManager

--- deck_manager.gd
+++ deck_manager.gd
@@ -1,10 +1,10 @@
-class_name CardManager
+class_name DeckManager
 
 var deck: Deck
 var _default_card_ids: Array[int] = [1, 2]
 
 func _init():
-	deck = default_deck()
+	deck = _default_deck()
 
 func _default_deck() -> Deck:
 	return Deck.new(_default_card_ids)
